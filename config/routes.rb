56a28c808824c68428a9bcc248c2cc44d4da15e6
EC2 Default User <ec2-user@ip-172-31-47-22.ap-northeast-1.compute.internal>
2023-08-18 00:17:36 +0000

    [Add]コントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'homes/new'
+  get 'homes/index'
+  get 'homes/show'
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'users/show'
+  get 'users/edit'
+  devise_for :books
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
